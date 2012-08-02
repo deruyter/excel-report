48db83296b2996bbeea5cab2a254a7a444ed70e6
Thomas Deruyter <thomas.deruyter@st.com>
2012-08-02 15:18:06 +0200

    new files

---
+++ contrib/gerrit/config.sh
@@ -0,0 +1,9 @@
+#
+# Default configuration for gerrit reviews.
+# Can be overriden by a local.sh file in the same directory.
+#
+server=ssh://gnx5855.gnb.st.com:29418
+project=<Your project>
+branch=master
+reviewers="christophe.guillon@st.com thomas.thery@st.com jeremy.bernard@st.com bruno.lavigueur@st.com"
+remote=gerrit
